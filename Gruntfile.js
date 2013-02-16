0174cfed9f620e899e505a06652d8acad7d26aad
Illution EC2 <root@illution.dk>
2013-02-16 20:44:16 +0000

    Grunt...

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,15 @@
 module.exports = function(grunt) {
 	pkg: grunt.file.readJSON('package.json'),
 	grunt.initConfig({
+		uglify: {
+			js: {
+				src: 'js/script.js',
+				dest: 'js/script.min.js'
+			}
+		},
 		concat: {
 			js: {
-				src: 'js/*.js',
+				src: 'js/*.min.js',
 				dest: 'js/concat.js'
 			},
 			css: {
@@ -11,21 +17,22 @@ module.exports = function(grunt) {
 				dest: 'css/concat.css'
 			}
 		},
-		uglify: {
-			js: {
-				src: 'js/concat.js',
-				dest: 'js/compiled.js'
-			}
-		},
 		cssmin: {
 			css: {
 				src: 'css/concat.css',
 				dest: 'css/compiled.css'
+			}
+		},
+		htmlrefs: {
+			html: {
+				src: 'index.html',
+				dest: 'index.compiled.html'
 			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-css');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
+	grunt.loadNpmTasks('grunt-htmlrefs');
+	grunt.registerTask('default', ['uglify', 'concat', 'cssmin', 'htmlrefs']);
 };
